ba1b05f319d7927da78db79ba54d2ee6718538f9
Sorin Sandru <sorin.sandru@idono.net>
2021-01-03 18:03:02 +0100

    Added: Logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,37 +1,54 @@
-import winston from 'winston';
+import winston, { format, LogEntry } from 'winston';
+import chalk from 'chalk';
 
-let logger: winston.Logger;
+import type { LoggingLevels, EnumLoggingLevels, LoggingOptions } from '~/types';
 
-export function createLogger(): void {
+export const Levels: EnumLoggingLevels = Object.freeze({
+  ERROR: 'error',
+  WARN: 'warn',
+  INFO: 'info',
+  VERBOSE: 'verbose',
+  DEBUG: 'debug',
+  SILLY: 'silly',
+});
+
+const formatter = format.printf(({ level, message }: LogEntry): string => {
+  switch (level) {
+    case 'error':
+      return chalk`{bgRed.black.bold Perkulator Error} {red ${message}}`;
+    case 'warn':
+      return chalk`{bgYellow.black.bold Perkulator Warning} {yellow ${message}}`;
+    case 'info':
+      return chalk`{bgGreen.black.bold Perkulator Info} {green ${message}}`;
+    case 'debug':
+      return chalk`{bgBlue.black.bold Perkulator Debug} {blue ${message}}`;
+    default:
+      return chalk`{bgWhite.black.bold Perkulator Verbose} {white ${message}}`;
+  }
+});
+
+const levels: Record<LoggingLevels, number> = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  verbose: 3,
+  debug: 4,
+  silly: 5,
+};
+
+export let logger: winston.Logger = configureLogger();
+
+export function configureLogger(options: LoggingOptions = {}): winston.Logger {
+  const { level = Levels.INFO, silent = false }: LoggingOptions = options;
+
   logger = winston.createLogger({
-    level: 'info',
-    levels: winston.config.npm.levels,
-    transports: new winston.transports.Console(),
     exitOnError: false,
-    silent: false,
+    format: formatter,
+    level,
+    levels: levels,
+    silent,
+    transports: new winston.transports.Console(),
   });
-}
 
-export function error(message: string): void {
-  logger.log({ level: 'error', message });
-}
-
-export function warn(message: string): void {
-  logger.log({ level: 'warn', message });
-}
-
-export function info(message: string): void {
-  logger.log({ level: 'info', message });
-}
-
-export function verbose(message: string): void {
-  logger.log({ level: 'verbose', message });
-}
-
-export function debug(message: string): void {
-  logger.log({ level: 'debug', message });
-}
-
-export function silly(message: string): void {
-  logger.log({ level: 'error', message });
+  return logger;
 }
--- src/types.ts
+++ src/types.ts
@@ -18,3 +18,20 @@ export type FileWatcherOptions = {
 } & WatchOptions;
 
 export type OnChangeEvent = (paths: readonly string[]) => void;
+
+export type LoggingLevels =
+  | 'error'
+  | 'warn'
+  | 'info'
+  | 'verbose'
+  | 'debug'
+  | 'silly';
+
+export type EnumLoggingLevels = {
+  readonly [P in Uppercase<LoggingLevels>]: Lowercase<P>;
+};
+
+export interface LoggingOptions {
+  level?: LoggingLevels;
+  silent?: boolean;
+}
