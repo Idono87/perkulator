4eb9fb64bc9798507c643d36a6fa5fbb7b13f664
Sorin Sandru <sorin.sandru@idono.net>
2021-01-10 20:56:46 +0100

    refactored task terminatino tests

--- src/__tests__/test-utils.ts
+++ src/__tests__/test-utils.ts
@@ -3,3 +3,54 @@ export async function wait(time = 100): Promise<void> {
     setTimeout(resolve, time);
   });
 }
+
+type RejectFunction = (reason?: any) => void;
+type ResolveFunction<T> = (value: T) => void;
+interface FakePromise<T> extends Promise<T> {
+  reject: RejectFunction;
+  resolve: ResolveFunction<T>;
+}
+
+const unresolvedFakePromises: Set<FakePromise<void>> = new Set();
+
+/**
+ * Create a fake promise
+ *
+ * @internal
+ */
+export function createFakePromise<T = any>(): FakePromise<T> {
+  let _reject: RejectFunction;
+  let _resolve: ResolveFunction<void>;
+
+  const pendingPromise: any = new Promise((resolve, reject) => {
+    _reject = reject;
+    _resolve = resolve;
+  });
+
+  pendingPromise.reject = () => {
+    unresolvedFakePromises.delete(pendingPromise);
+    _reject();
+  };
+
+  pendingPromise.resolve = () => {
+    unresolvedFakePromises.delete(pendingPromise);
+    _resolve();
+  };
+
+  unresolvedFakePromises.add(pendingPromise);
+
+  return pendingPromise;
+}
+
+/**
+ * Resolves all hanging promises.
+ *
+ * @internal
+ */
+export function resolveFakePromises(): void {
+  for (const promise of unresolvedFakePromises) {
+    promise.resolve();
+  }
+
+  unresolvedFakePromises.clear();
+}
--- src/task/__tests__/task.test.ts
+++ src/task/__tests__/task.test.ts
@@ -1,5 +1,5 @@
 import { expect, use } from 'chai';
-import { createSandbox, SinonStub } from 'sinon';
+import { createSandbox, SinonFakeTimers, SinonStub } from 'sinon';
 import ChaiAsPromised from 'chai-as-promised';
 
 import Task from '~/task/task';
@@ -8,6 +8,7 @@ import TaskModuleNotFoundError from '~/errors/task-module-not-found-error';
 import InvalidRunnableTaskError from '~/errors/invalid-runnable-task-error';
 import TaskTerminationTimeoutError from '~/errors/task-termination-timeout-error';
 import MissingInterfaceError from '~/errors/missing-interface-error';
+import { createFakePromise, resolveFakePromises } from '~/__tests__/test-utils';
 
 use(ChaiAsPromised);
 
@@ -46,33 +47,6 @@ describe('Task', function () {
     return expect(task.run()).to.be.fulfilled;
   });
 
-  it('Expect task to be stopped', async function () {
-    let res: any;
-
-    runTask = Sinon.stub().callsFake(
-      async (): Promise<void> => {
-        await new Promise((resolve, reject) => {
-          res = resolve;
-        });
-      },
-    );
-
-    stopTask = Sinon.stub().callsFake(() => {
-      res();
-      return true;
-    });
-
-    const path = __filename;
-    const options: TaskOptions = {
-      path,
-    };
-    const task = Task.createTask(options);
-    const promise = task.run();
-
-    await expect(task.stop()).to.be.fulfilled;
-    return expect(promise).to.be.fulfilled;
-  });
-
   it(`Expect run to throw a "${TaskModuleNotFoundError.name}" if module doesn't exist`, function () {
     const path = './not/a/real/module';
     const options: TaskOptions = {
@@ -94,63 +68,90 @@ describe('Task', function () {
     expect(() => Task.createTask(options)).to.throw(InvalidRunnableTaskError);
   });
 
-  it(`Expect to throw ${TaskTerminationTimeoutError.name} when task termination times out.
-  Unresolved "stopTask"`, function () {
-    runTask = Sinon.stub().returns(
-      new Promise<void>((resolve, reject) => {
-        setTimeout(resolve, 10000);
-      }),
-    );
+  describe('Task termination tests', function () {
+    let timer: SinonFakeTimers;
 
-    stopTask = Sinon.stub().returns(
-      new Promise<void>((resolve, reject) => {
-        setTimeout(resolve, 10000);
-      }),
-    );
+    beforeEach(() => {
+      timer = Sinon.useFakeTimers();
+    });
 
-    const path = __filename;
-    const options: TaskOptions = {
-      path,
-    };
-    const task = Task.createTask(options);
-    void task.run();
-    return expect(task.stop()).to.be.rejectedWith(TaskTerminationTimeoutError);
-  });
+    afterEach(() => {
+      timer.restore();
+      resolveFakePromises();
+    });
 
-  it(`Expect to throw ${TaskTerminationTimeoutError.name} when task termination times out. 
-  Unresolved "runTask"`, function () {
-    runTask = Sinon.stub().returns(
-      new Promise<void>((resolve, reject) => {
-        setTimeout(resolve, 10000);
-      }),
-    );
+    it('Expect task to be stopped', async function () {
+      let res: any;
 
-    stopTask = Sinon.stub().returns(Promise.resolve());
+      runTask = Sinon.stub().callsFake(
+        async (): Promise<void> => {
+          await new Promise((resolve, reject) => {
+            res = resolve;
+          });
+        },
+      );
 
-    const path = __filename;
-    const options: TaskOptions = {
-      path,
-    };
-    const task = Task.createTask(options);
-    void task.run();
-    return expect(task.stop()).to.be.rejectedWith(TaskTerminationTimeoutError);
-  });
+      stopTask = Sinon.stub().callsFake(() => {
+        res();
+        return true;
+      });
 
-  it(`Expect to throw ${MissingInterfaceError.name} when interface function "stopTask" isn't implemented`, function () {
-    runTask = Sinon.stub().returns(
-      new Promise<void>((resolve, reject) => {
-        setTimeout(resolve, 10000);
-      }),
-    );
+      const path = __filename;
+      const options: TaskOptions = {
+        path,
+      };
+      const task = Task.createTask(options);
+      const promise = task.run();
 
-    stopTask = undefined;
+      await expect(task.stop()).to.be.fulfilled;
+      return expect(promise).to.be.fulfilled;
+    });
 
-    const path = __filename;
-    const options: TaskOptions = {
-      path,
-    };
-    const task = Task.createTask(options);
-    void task.run();
-    return expect(task.stop()).to.be.rejectedWith(MissingInterfaceError);
+    it(`Expect to throw ${TaskTerminationTimeoutError.name} when pending stop request doesn't resolve before the timeout.`, function () {
+      runTask = Sinon.stub().returns(createFakePromise());
+      stopTask = Sinon.stub().returns(createFakePromise());
+
+      const path = __filename;
+      const options: TaskOptions = {
+        path,
+      };
+      const task = Task.createTask(options);
+      void task.run();
+
+      void timer.nextAsync();
+      return expect(task.stop()).to.be.rejectedWith(
+        TaskTerminationTimeoutError,
+      );
+    });
+
+    it(`Expect to throw ${TaskTerminationTimeoutError.name} when pending task run doesn't terminate before the timeout`, function () {
+      runTask = Sinon.stub().returns(createFakePromise());
+      stopTask = Sinon.stub().returns(Promise.resolve());
+
+      const path = __filename;
+      const options: TaskOptions = {
+        path,
+      };
+      const task = Task.createTask(options);
+      void task.run();
+
+      void timer.nextAsync();
+      return expect(task.stop()).to.be.rejectedWith(
+        TaskTerminationTimeoutError,
+      );
+    });
+
+    it(`Expect to throw ${MissingInterfaceError.name} when interface function "stopTask" isn't implemented`, function () {
+      runTask = Sinon.stub().returns(createFakePromise());
+      stopTask = undefined;
+
+      const path = __filename;
+      const options: TaskOptions = {
+        path,
+      };
+      const task = Task.createTask(options);
+      void task.run();
+      return expect(task.stop()).to.be.rejectedWith(MissingInterfaceError);
+    });
   });
 });
