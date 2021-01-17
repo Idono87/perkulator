3944c702efca7294851948b4d5172f38670c3854
Sorin Sandru <sorin.sandru@idono.net>
2021-01-17 22:00:02 +0100

    change validation test to use utility function

--- src/config/__tests__/validation.test.ts
+++ src/config/__tests__/validation.test.ts
@@ -1,25 +1,28 @@
 import { expect } from 'chai';
 
-import { PerkulatorOptions } from '~/types';
 import validateOptions from '~/config/validation';
 import ValidationError from '~/errors/validation-error';
+import { createPerkulatorOptions } from '~/__tests__/utils';
+import type { PerkulatorOptions } from '~/types';
 
 describe('Configuration Validation', function () {
   describe('paths', function () {
     it('Expect string array to pass', function () {
-      const options: PerkulatorOptions = { paths: ['/test/path'] };
+      const options: PerkulatorOptions = createPerkulatorOptions();
 
       expect(() => validateOptions(options)).to.not.throw(ValidationError);
     });
 
     it('Expect non-array to fail', function () {
-      const options: PerkulatorOptions = { paths: {} as any };
+      const options: PerkulatorOptions = createPerkulatorOptions();
+      options.paths = {} as any;
 
       expect(() => validateOptions(options)).to.throw(ValidationError);
     });
 
     it('Expect non-string array to fail', function () {
-      const options: PerkulatorOptions = { paths: [12345 as any] };
+      const options: PerkulatorOptions = createPerkulatorOptions();
+      options.paths = [123456 as any];
 
       expect(() => validateOptions(options)).to.throw(ValidationError);
     });
