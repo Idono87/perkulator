f38235029c8f805794ca3f6176b69cf2cd881b49
Sorin Sandru <sorin.sandru@idono.net>
2021-01-31 19:27:08 +0100

    refactored name to timeout error

--- src/errors/task-termination-timeout-error.ts
+++
@@ -1,7 +0,0 @@
-import BaseError from './base-error';
-
-export default class TaskTerminationTimeoutError extends BaseError {
-  public constructor() {
-    super(`Failed to terminate task. Termination timed out.`);
-  }
-}
